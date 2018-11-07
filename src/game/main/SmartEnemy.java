f11a03353186784c9841b014d7f794b7438d5ea9
mitch lindsey <mlindsey19@gmail.com>
2018-11-07 09:57:31 -0600

    smrtnme

--- src/game/main/SmartEnemy.java
+++ src/game/main/SmartEnemy.java
@@ -6,28 +6,37 @@ public class SmartEnemy extends GameObject {
 
     private GameObject player;
 
-    public SmartEnemy(int x, int y, ID id, Handler handler) {
+    public SmartEnemy(float x, float y, ID id, Handler handler) {
         super(x, y, id, handler);
 
-        player = handler.objects.getFirst();
+        player = handler.objects.getFirst(); //currently player will always be the first object
 
     }
     @Override
     public Rectangle getBounds(){
-        return new Rectangle(x,y,16,16);
+        return new Rectangle( (int) x, (int) y,16,16);
     }
     @Override
     public void tick() {
-        moveWithinWindow();
+        moveWithinWindow(); //
 
-        handler.addObject(new Trail(x,y, ID.Trail, handler, Color.red, 16,16, 0.1f));
+        double xDistSquared = ( x - player.getX() ) * ( x - player.getX() );
+        double yDistSquared = ( y - player.getY() ) * ( y - player.getY() );
+        float diffX = x - player.getX() - 8;
+        float diffY = y - player.getY() - 8;
+        double distance = Math.sqrt( xDistSquared + yDistSquared );
+
+        velocityX = (float) (( -1.0 / distance) * diffX);
+        velocityY = (float) (( -1.0 / distance) * diffY);
+
+        handler.addObject(new Trail(  x,  y, ID.Trail, handler, Color.green, 16,16, 0.1f));
 
     }
 
     @Override
     public void render(Graphics g) {
-        g.setColor(Color.red);
-        g.fillRect(x,y, 16,16);
+        g.setColor(Color.green);
+        g.fillRect( (int) x, (int) y, 16,16);
 
     }
 }
